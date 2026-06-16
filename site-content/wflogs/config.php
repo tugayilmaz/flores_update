be0d13743b77647addf22a6ab71586f6e1d1a9cd
tugayilmaz <tugay_yilmaz_02@hotmail.com>
2026-06-16 03:04:51 +0300

    Remove unnecessary logs, cache and language files

--- site-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"~7/L$eSU}@[KC/VKI4;29dEd=RTf`x3Z]jf_pTlJj$f{P248!je5X{!d(7/j;);h";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:583;}
